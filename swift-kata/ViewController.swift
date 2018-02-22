44797d65d3089261dba3b7ba9d489212fc12643f
Jessica Le <jessicaduongle@gmail.com>
2018-02-21 19:15:03 -0600

    Initial Commit

---
+++ swift-kata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-kata
+//
+//  Created by Jessica Le on 2/21/18.
+//  Copyright © 2018 lovelejess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
